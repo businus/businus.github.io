d31afec7a53111581e916d94bc61da7728a1cbb2
Shiv Patil <shivgeeky@gmail.com>
2025-10-08 03:49:08 +0530

    lala

--- frontend/src/routes/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PremiumLandingPage from '../components/PremiuimStuff/layout/PremiumLandingPage';
-
-export function Index() {
-  return <PremiumLandingPage />;
-}
-
-export default Index;
